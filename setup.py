bd20ac486067c0846b19c94c0da5d30e0bd35c47
Eduard Poliakov <ya.polykov@gmail.com>
2023-10-12 19:35:58 +0300

    add 1.0.0.14 version to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@ from setuptools import find_packages, setup
 with open("requirements.txt", "r", encoding="utf-8") as req_file:
     install_requires = req_file.read().splitlines()
 
-version = "1.0.3.13"
+with open("README.md", "r", encoding="utf-8") as readme_file:
+    readme = readme_file.read()
+
+version = "1.0.3.14"
 
 setup(
     name="demand_forecast_source",
@@ -11,6 +14,7 @@ setup(
     package_dir={"": "src"},
     version=version,
     description="This is lib-predicts for demand forecast based on machine learning",
+    long_description=readme,
     author="Eduard Poliakov",
     author_email="ya.polykov@gmail.com",
     license="MIT",
